2e4e1923ab75f5298cc8ed9c5eee44f46505cc14
Shajaan Balasingam <shajaan.balasingam@technolibre.ca>
2023-03-31 11:56:19 -0400

    [ADD] aliment_app: Ajout d'une application dans l'intranet (Web) pour ajouter, afficher, supprimer et modifier un aliment

---
+++ aliment_app/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Aliment App",
+    "category": "Web",
+    "version": "12.0.1.0",
+    "author": "TechnoLibre",
+    "license": "AGPL-3",
+    "website": "https://technolibre.ca",
+    "application": True,
+    "depends": ["mail"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/aliment_internal.xml",
+        "views/menu.xml",
+    ],
+    "installable": True,
+}
